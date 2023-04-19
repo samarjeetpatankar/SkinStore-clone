38a3e94c13770ad50d5b7139bbe6e8c3ec72d8e3
ravee25 <jaiswalravi74254@gmail.com>
2023-04-19 17:32:56 +0530

    adding product page

---
+++ productPage.js
@@ -0,0 +1,332 @@
+
+var productData = [
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11535230-2295043599450781.jpg",
+        name: "Christophe Robin Delicate Volumising Conditioner with Rose Extracts 200ml",
+        box: "25% off with code LUCKY",
+        price: "39.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11732894-2094931106360459.jpg",
+        name: "Daniel Sandler Watercolour Liquid Cheek Colour 15ml (Various Shades)",
+        box: "30% off with code DANIEL",
+        price: "25.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13971627-1124998491824470.jpg",
+        name: "Deck Of Scarlet Big Lash Energy - Long & Strong Mascara",
+        box: "20% off with code SCAR",
+        price: "28.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13507087-5975032476504174.jpg",
+        name: "Wander Beauty Unlashed Volume and Curl Mascara - Tarmac 9g",
+        box: "15% off with code WANDE",
+        price: "27.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13996095-8595031482300943.jpg",
+        name: "Elizabeth Arden Blockbuster Set",
+        box: "40% off with code ARDEN",
+        price: "42.00",
+
+    },
+    
+
+
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13411317-1324994437377264.jpg",
+        name: "Christophe Robin New Dreamy Volume Ritual",
+        box: "25% off with code LUCKY",
+        price: "62.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/10553426-8114931104872981.jpg",
+        name: "Daniel Sandler Waterbrush",
+        box: "30% off with code DANIEL",
+        price: "25.00",
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13971582-9865000417132418.jpg",
+        name: "Deck Of Scarlet Dual Drama Liquid Eyeliner (Various Shades)",
+        box: "20% off with code SCAR",
+        price: "28.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13482284-4384963508334210.jpg",
+        name: "Wander Beauty Lip Retreat Oil 20ml (Various Shades)",
+        box: "15% off with code WANDE",
+        price: "22.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11313646-1994918723914534.jpg",
+        name: "Elizabeth Arden Beautiful Color Bold Liquid Lipstick (Various Colors)",
+        box: "40% off with code ARDEN",
+        price: "16.50",
+
+    },
+
+
+
+
+     {
+        image_url: "https://static.thcdn.com/images/large/webp/productimg/960/960/10556828-1984358354248728.jpg",
+        name: "Christophe Robin Antioxidant Conditioner with 4 Oils and Blueberry (8.7oz)",
+        box: "25% off with code LUCKY",
+        price: "21.50",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12436045-3134746401363193.jpg",
+        name: "Daniel Sandler Anti-Redness Foundation Primer",
+        box: "30% off with code DANIEL",
+        price: "37.50",
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13971596-1904998482750547.jpg",
+        name: "Deck Of Scarlet Mirror Glaze Highlighting Trio (Various Shades)",
+        box: "20% off with code SCAR",
+        price: "36.00"
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12219339-1204897449786360.jpg",
+        name: "Wander Beauty Wanderlust Powder Foundation 0.31 oz (Various Shades)",
+        box: "15% off with code WANDE",
+        price: "40.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13461927-1144911419829243.jpg",
+        name: "Elizabeth Arden Exclusive Red Carpet Ready Lashes Set",
+        box: "40% off with code ARDEN",
+        price: "125.00",
+    },
+
+
+
+
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12635432-1134893265973415.jpg",
+        name: "Christophe Robin Purifying Conditioner Gelée with Sea Minerals 200ml",
+        box: "25% off with code LUCKY",
+        price: "15.60",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11925928-1214931106708867.jpg",
+        name: "Daniel Sandler Watercolour Liquid Customisable Colour Set for Cheeks",
+        box: "30% off with code DANIEL",
+        price: "66.00",
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13971628-1364998481061634.jpg",
+        name: "Deck Of Scarlet Double Take Face Brush",
+        box: "20% off with code SCAR",
+        price: "32.00"
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13909150-1984975453071900.jpg",
+        name: "Wander Beauty Getaway Eye and Face Palette - Island Hop",
+        box: "15% off with code WANDE",
+        price: "44.00",
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/10801127-9564918723292010.jpg",
+        name: "Elizabeth Arden Flawless Finish Sponge On Cream Makeup (23g)",
+        box: "40% off with code ARDEN",
+        price: "30.00",
+
+    },
+
+
+
+
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12635425-2054893266456808.jpg",
+        name: "Christophe Robin Hydrating Leave-in Mist with Aloe Vera 150ml",
+        box: "25% off with code LUCKY",
+        price: "39.00",
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12436040-2094739621888766.jpg",
+        name: "Daniel Sandler Watercolour Gel Cheek Colour 10ml (Various Shades)",
+        box: "30% off with code DANIEL",
+        price: "27.50",
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13971588-5624998481683801.jpg",
+        name: "Deck Of Scarlet Metal Leaf Shadow Pot (Various Shades)",
+        box: "20% off with code SCAR",
+        price: "25.00",
+    },
+    {
+        image_url: "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12906102-2084866440066073.jpg",
+        name: "Wander Beauty Skyliner Automatic Gel Eyeliner 0.5 g.",
+        box: "15% off with code WANDE",
+        price: "21.00",
+    },
+    
+
+];
+
+
+// var cartArray = JSON.parse(localStorage.getItem("cartItems")) || [];
+// var wishArray = JSON.parse(localStorage.getItem("wishItems")) || [];
+reLoadM(productData);
+
+function reLoadM(productData){
+
+
+// console.log(productData)
+
+
+// DATA
+
+ displayDatas(productData);
+function displayDatas(productData){
+    document.querySelector("#container").textContent="";
+    productData.map(function (data) {
+        var div = document.createElement("div")
+        
+    
+        // inside div creating image and  image_url
+        var image = document.createElement("img");
+        image.setAttribute("src", data.image_url);
+        div.append(image);
+    
+    
+        var name = document.createElement("p");
+        name.style.height = "35px"
+        name.textContent = data.name;
+        name.style.marginTop = "3%"
+        div.append(name);
+    
+        var box = document.createElement("div")
+        box.textContent = data.box
+        box.setAttribute("id", "boxer")
+        div.append(box)
+    
+    
+        var p2 = document.createElement("p")
+        p2.textContent = `$${data.price}`;
+        p2.style.marginTop = "3%"
+        p2.style.fontSize = "24px"
+        p2.style.fontWeight = "bold"
+        div.append(p2)
+    
+    
+    
+        var btn1 = document.createElement("button")
+        btn1.setAttribute("id", "btnwish")
+        btn1.textContent = "Add to Wishlist"
+        btn1.style.display = "block"
+        btn1.addEventListener("click", function () {
+            wishlistAdd(data);
+        })
+        div.append(btn1)
+    
+    
+        var btn2 = document.createElement("button")
+        btn2.setAttribute("id", "btnbuy");
+        btn2.textContent = "QUICK BUY"
+        btn2.addEventListener("click", function () {
+            cartAdd(data);
+        })
+    
+        div.append(btn2);
+        div.setAttribute("id", "Products-Details")
+        image.addEventListener("click", function () {
+            sendToDetails(data);
+        })
+        document.querySelector("#container").append(div)
+    
+    })
+    
+}
+
+
+var cartThings = JSON.parse(localStorage.getItem("cartthings")) || [];
+var cartArray = JSON.parse(localStorage.getItem("quickbuy1")) || [];
+function wishlistAdd(data) {
+    cartArray.push(data);
+    cartThings.push(data);
+    localStorage.setItem("quickbuy1", JSON.stringify(cartArray));
+    localStorage.setItem("cartthings", JSON.stringify(cartThings));
+    alert("Item Added to WishList");
+    reLoadM(productData);
+
+}
+
+
+function cartAdd(data) {
+  
+    cartThings.push(data);
+  
+    localStorage.setItem("cartthings", JSON.stringify(cartThings));
+    alert("Item Added to cart");
+    reLoadM(productData);
+
+}
+
+
+var cartLength =JSON.parse(localStorage.getItem("cartthings"))
+document.querySelector("#cardAval").innerHTML=`There are currently ${cartLength.length} items in your cart.`;
+document.querySelector("#cartLen").innerHTML=`${cartLength.length}`;
+
+}
+
+
+console.log(cartArray);
+console.log(cartArray);
+
+// Sorting Function 
+
+function Tsort(){
+    var selectedsort =document.querySelector("#sortT").value
+   
+    if(selectedsort === "high"){
+        productData = productData.sort(function(a,b){
+            return b.price - a.price
+        })
+    }
+    if(selectedsort === "low"){
+        productData = productData.sort(function(a,b){
+            return a.price - b.price
+        })
+    }
+    reLoadM(productData)
+} 
+
+
+// Filter Function
+
+ function filterit(){
+    
+    var selected = document.querySelector("#filter").value
+
+    var filteredArr = productData.filter(function(ele){
+        if(selected === ""){
+            return 1
+        }
+        else{
+            return selected === ele.box
+        }
+    })
+    reLoadM(filteredArr);
+
+   }
+
+
+ 
+   function sendToDetails(data){
+      localStorage.setItem("ProductDetails", JSON.stringify(data));
+      window.location.href='detail.html'
+   }
